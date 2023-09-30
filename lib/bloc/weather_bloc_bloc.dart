9b92245bdb913d66e73ca846078da544386e65dd
Suraj Yadav <90672206+suraj-yadav0@users.noreply.github.com>
2023-09-30 11:38:13 +0530

    initiial commit

---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:weather/weather.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
+  WeatherBlocBloc() : super(WeatherBlocInitial()) {
+    on<WeatherBlocEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/bloc/weather_bloc_event.dart
@@ -0,0 +1,13 @@
+part of 'weather_bloc_bloc.dart';
+
+sealed class WeatherBlocEvent extends Equatable {
+  const WeatherBlocEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+
+class FetchWeather extends WeatherBlocEvent {
+  
+}
